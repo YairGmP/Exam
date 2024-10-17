e449e482eccc1eb366f75812f290fc49110f18e9
YairGmP <2302133@utrivieramaya.edu.mx>
2024-10-17 09:14:48 -0500

    Examen 17/oct/2024

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 document.getElementById('year').textContent = new Date().getFullYear();
+
+const value = document.querySelector('#range_value');
+const input = document.querySelector('#range');
+value.textContent = input.value;
+input.addEventListener("input", (event) =>{
+    value.textContent = event.target.value;
+});
